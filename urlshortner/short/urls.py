596e552b1e141f4e4ec9540731e2c116027ec256
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-05-18 16:04:03 +0100

    redirect working

--- urlshortner/short/urls.py
+++ urlshortner/short/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shortener, name='shortener' )
+    path('', views.shortener, name='shortener' ),
+    path('<str:shortened_link>/', views.link_redirect, name="redirect")
 ]
